49ea712d6bcf098f160a7332ec21db813da5c045
L00171222 <L00171222@atu.ie>
2023-03-20 21:57:53 +0000

    code commmented

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Logo from './movie.jpg'
 
 const Home = () => {
   //This Home Page Studio Ghibil Website
   return (
     <div>
       <h2>This is the home page! </h2>
+      <img src={Logo}/>
       
 
     </div>
