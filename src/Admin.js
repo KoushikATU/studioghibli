b6219a22a648a9c4a09406bee768dd0859749be3
L00171222 <L00171222@atu.ie>
2023-03-20 21:42:44 +0000

    code commmented

--- src/Admin.js
+++ src/Admin.js
@@ -3,20 +3,21 @@ import { useEffect, useState } from 'react';
 
 function Admin() {
     const [data, setData]= useState([])
+
+    // useEffect is a React Hook that runs once on component load.
     useEffect(()=>{
-    axios.get('https://jei9r6lp78.execute-api.us-east-1.amazonaws.com/prod/products')
-        .then(res =>{
-            console.log(res)
-            setData(res?.data?.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+        // This makes an API call to retrieve the products data.
+        axios.get('https://jei9r6lp78.execute-api.us-east-1.amazonaws.com/prod/products')
+            .then(res =>{
+                console.log(res)
+                setData(res?.data?.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     },[])
 
-    
-
-
+    // This displays the data in a table.
     return (
         <div>
             Table of Data:
@@ -28,67 +29,45 @@ function Admin() {
                     <th>Course</th>
                     <th>Extra Requests</th>
                     <th>Actions</th>
-                   
-                    
                 </tr>
-        {
-        data.length > 0 &&
-        data.map((item, index)=>{
-            
-            
-            
-            const Delete = () =>{
-                console.log(item.username);
-                
-                const deleteUsername = {
-                    "username": item.username,
-                    
-                    }
-                console.log(deleteUsername);
-
-                axios.post('https://jei9r6lp78.execute-api.us-east-1.amazonaws.com/prod/products',deleteUsername)
-                .then(res=>{
-                  console.log(res.data) 
-                })
-                .catch(
-                    err => {
-                        console.log(err)
+                {
+                    // This maps over the data retrieved from the API and displays it in the table.
+                    data.length > 0 &&
+                    data.map((item, index)=>{
+                        // This is a helper function to delete a specific product.
+                        const Delete = () =>{
+                            console.log(item.username);
+                            const deleteUsername = {
+                                "username": item.username,
+                            }
+                            console.log(deleteUsername);
+                            // This makes an API call to delete the specified product.
+                            axios.post('https://jei9r6lp78.execute-api.us-east-1.amazonaws.com/prod/products',deleteUsername)
+                            .then(res=>{
+                              console.log(res.data) 
+                            })
+                            .catch(
+                                err => {
+                                    console.log(err)
+                                })
+                        }
+                        return (
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.username}</td>
+                                <td>{item.email}</td>
+                                <td>Moving Castle Creations</td>
+                                <td></td>
+                                <td>
+                                    <button onClick={Delete}>Delete</button>
+                                </td>
+                            </tr>
+                        )
                     })
-
-                // )
-                // axios.get('https://jei9r6lp78.execute-api.us-east-1.amazonaws.com/prod/products')
-                // .then(res =>{
-                //  console.log(res)
-                // //setData(res?.data?.data)
-                //  })
-                //  .catch(err => {
-                // console.log(err)
-                //  })
-                
-        
-            }
-          return (
-            <tr>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
-                <td>Moving Castle Creations</td>
-                <td></td>
-                
-                <td>
-                    <button onClick={Delete}>Delete</button>
-                </td>
-                
-            </tr>
-            
-          )
-        })
-      }
-        </table>
-       
+                }
+            </table>
         </div>
-    )
-    
+    )    
 }
 
 export default Admin;
